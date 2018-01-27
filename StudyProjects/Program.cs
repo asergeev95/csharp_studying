3c1c17971e84634eb828465c17e4291beeae4e05
A.Sergeev <aesergnsk@gmail.com>
2018-01-27 20:46:10 +0700

    Добавьте файлы проекта.

---
+++ StudyProjects/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudyProjects
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
